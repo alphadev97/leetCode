edd3a9714ed2c9ab263e3d71ca8191f44967a185
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-07 23:26:16 +0500

    122. Best Time to Buy and Sell Stock II

---
+++ Top-Interview-150/122.BestTimetoBuyandSellStockII.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (minPrice > prices[i]) {
+      minPrice = prices[i];
+    }
+
+    if (prices[i] - minPrice > 0) {
+      maxProfit = maxProfit + prices[i] - minPrice;
+      minPrice = prices[i];
+    }
+  }
+
+  return maxProfit;
+};
