c569555d6c4bf74b1a08ff53f35b920c7fb31c5c
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-07 20:42:17 +0500

    189. Rotate Array

--- Top-Interview-150/189.RotateArray.js
+++ Top-Interview-150/189.RotateArray.js
@@ -7,6 +7,6 @@ var rotate = function (nums, k) {
   if (nums.length < k) {
     k = k % nums.length;
   }
-  let shapatar = nums.splice(0, nums.length - k);
-  return nums.push(...shapatar);
+  let i = nums.splice(0, nums.length - k);
+  return nums.push(...i);
 };
