a2ce4d2fd4d18a209cdc14c105250759aba007b0
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-09 20:21:09 +0500

    45. Jump Game II

--- Top-Interview-150/45.JumpGameII.js
+++ Top-Interview-150/45.JumpGameII.js
@@ -3,23 +3,21 @@
  * @return {number}
  */
 var jump = function (nums) {
-  let minJump = nums[0];
+  let maxJump = 0;
+  let pMaxJump = nums[0];
   let counter = 1;
-  let arr = [];
-  let index = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (i > index + nums[index]) {
-      index = Math.max(...arr);
+  if (nums.length < 2) {
+    return 0;
+  }
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    maxJump = Math.max(maxJump, i + nums[i]);
+
+    if (i >= pMaxJump) {
+      pMaxJump = maxJump;
       counter++;
     }
-
-    if (index + nums[index] < i + nums[i]) {
-      arr.push(i + nums[i]);
-    }
-
-    if (index + nums[index] > nums.length) {
-      return counter;
-    }
   }
+  return counter;
 };
