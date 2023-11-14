e7410b8a83da9d061d5e9be55c4599072d81cc2d
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-14 19:36:39 +0500

    380. Insert Delete GetRandom O(1)

--- Top-Interview-150/380.InsertDeleteGetRandomO(1).js
+++ Top-Interview-150/380.InsertDeleteGetRandomO(1).js
@@ -1,21 +1,44 @@
-var RandomizedSet = function () {};
+var RandomizedSet = function () {
+  this.RandomizedSet = [];
+};
 
 /**
  * @param {number} val
  * @return {boolean}
  */
-RandomizedSet.prototype.insert = function (val) {};
+RandomizedSet.prototype.insert = function (val) {
+  if (this.RandomizedSet.includes(val)) {
+    return false;
+  }
+
+  this.RandomizedSet.push(val);
+
+  return true;
+};
 
 /**
  * @param {number} val
  * @return {boolean}
  */
-RandomizedSet.prototype.remove = function (val) {};
+RandomizedSet.prototype.remove = function (val) {
+  if (!this.RandomizedSet.includes(val)) {
+    return false;
+  }
+
+  const valIndex = this.RandomizedSet.indexOf(val);
+  this.RandomizedSet.splice(valIndex, 1);
+
+  return true;
+};
 
 /**
  * @return {number}
  */
-RandomizedSet.prototype.getRandom = function () {};
+RandomizedSet.prototype.getRandom = function () {
+  const index = Math.floor(Math.random() * this.RandomizedSet.length);
+
+  return this.RandomizedSet[index];
+};
 
 /**
  * Your RandomizedSet object will be instantiated and called as such:
