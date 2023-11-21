0eaa9b6204ced98de5c4329e227f6e3aedfd41bc
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-21 23:12:58 +0500

    238. Product of Array Except Self

--- Top-Interview-150/238.ProductofArrayExceptSelf.js
+++ Top-Interview-150/238.ProductofArrayExceptSelf.js
@@ -3,14 +3,27 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-  const answer = [];
-  let index = 0;
-  let value = 1;
+  let answer = [];
+  let left = [];
+  let right = [];
+  left[0] = 1;
+  right[nums.length - 1] = 1;
+  let total = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    total *= nums[i - 1];
+    left[i] = total;
+  }
+
+  total = 1;
+
+  for (let i = nums.length - 2; i >= 0; i--) {
+    total *= nums[i + 1];
+    right[i] = total;
+  }
 
   for (let i = 0; i < nums.length; i++) {
-    value = value * nums[i - index[i]];
-    answer.push(value);
-    index++;
+    answer[i] = left[i] * right[i];
   }
 
   return answer;
