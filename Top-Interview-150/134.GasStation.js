0029ba92525949dd31f4628f89324d6d02d3da17
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-22 17:49:56 +0500

    134. Gas Station

--- Top-Interview-150/134.GasStation.js
+++ Top-Interview-150/134.GasStation.js
@@ -4,29 +4,17 @@
  * @return {number}
  */
 var canCompleteCircuit = function (gas, cost) {
-  let tank = 0;
-  let gasInStation = gas[0];
-  let gasToAnotherStation = cost[0];
+  let currentTank = 0;
+  let totalTank = 0;
   let gasStation = 0;
 
   for (let i = 0; i < gas.length; i++) {
-    for (let n = i; n > -1; n++) {
-      if (n > gas.length - 1) {
-        n = 0;
-      }
-      tank += gas[i];
-      if (tank < cost[i]) {
-        tank = 0;
-        gasStation++;
-        break;
-      }
-
-      if (n === i - 1) {
-        if (tank < cost[n]) {
-          return -1;
-        }
-        return i;
-      }
+    currentTank += gas[i] - cost[i];
+    totalTank += gas[i] - cost[i];
+    if (currentTank < 0) {
+      currentTank = 0;
+      gasStation = i + 1;
     }
   }
+  return totalTank < 0 ? -1 : gasStation;
 };
