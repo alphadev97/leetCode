b44314cb727fc266e7f3986cfa060ae508343292
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-23 21:39:13 +0500

    135. Candy

--- Top-Interview-150/135.Candy.js
+++ Top-Interview-150/135.Candy.js
@@ -3,22 +3,38 @@
  * @return {number}
  */
 var candy = function (ratings) {
-  let total = ratings;
-  let candies = 0;
+  let total = [];
   for (let i = 0; i < ratings.length; i++) {
-    if (ratings[i] === total[i]) {
-      total[i]++;
-      candies++;
-    }
+    total[i] = 1;
+  }
+
+  for (let i = 0; i < ratings.length; i++) {
     if (ratings[i] > ratings[i - 1] && ratings[i] > ratings[i + 1]) {
-      candies++;
-    }
-    if (i === ratings.length - 1 && ratings[i] > ratings[i - 1]) {
-      candies++;
-    }
-    if (i === 0 && ratings[i] > ratings[i + 1]) {
-      candies++;
+      total[i] = Math.max(total[i - 1], total[i + 1]) + 1;
+    } else if (ratings[i] > ratings[i - 1]) {
+      total[i] = total[i - 1] + 1;
+    } else if (ratings[i] > ratings[i + 1]) {
+      total[i] = total[i + 1] + 1;
     }
   }
-  return candies;
+  for (let i = ratings.length - 1; i > -1; i--) {
+    if (ratings[i] > ratings[i - 1] && ratings[i] > ratings[i + 1]) {
+      total[i] = Math.max(total[i - 1], total[i + 1]) + 1;
+    } else if (ratings[i] > ratings[i - 1]) {
+      total[i] = total[i - 1] + 1;
+    } else if (ratings[i] > ratings[i + 1]) {
+      total[i] = total[i + 1] + 1;
+    }
+  }
+  if (ratings[0] > ratings[1]) {
+    total[0] = total[1] + 1;
+  }
+  if (ratings[ratings.length - 1] > ratings[ratings.length - 2]) {
+    total[ratings.length - 1] = total[ratings.length - 2] + 1;
+  }
+  let sum = total.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+  console.log(total);
+  return sum;
 };
