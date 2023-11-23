94370ee48e75002794fc78ea4f80c239a2c13f64
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-23 22:31:13 +0500

    42. Trapping Rain Water

--- Top-Interview-150/42.TrappingRainWater.js
+++ Top-Interview-150/42.TrappingRainWater.js
@@ -2,4 +2,20 @@
  * @param {number[]} height
  * @return {number}
  */
-var trap = function (height) {};
+var trap = function (height) {
+  let water = 0;
+  let n = height;
+
+  if (height[0] < 1) {
+    height.splice(0, 1);
+    return;
+  }
+
+  for (let i = 0; i < height.length; i++) {
+    if (height[i] > height[i - 1]) {
+      water++;
+    }
+  }
+
+  return water;
+};
